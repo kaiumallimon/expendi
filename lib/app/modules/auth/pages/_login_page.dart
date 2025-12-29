288692346d4689fafe0c2b6f454c913e5064827b
Kaium Al Limon <limon.3cs@gmail.com>
2025-12-29 22:55:21 +0600

    refactor: remove unused Cupertino import from login page

--- lib/app/modules/auth/pages/_login_page.dart
+++ lib/app/modules/auth/pages/_login_page.dart
@@ -1,6 +1,5 @@
 import 'package:expendi/app/modules/auth/logics/_login_cubit.dart';
 import 'package:expendi/app/shared/widgets/_app_logo.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
